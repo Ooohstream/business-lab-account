869f714875201109360d709433a5117ecbfaab5d
Ooohstream <69838796+Ooohstream@users.noreply.github.com>
2021-02-03 23:59:57 +0300

    semi-added login

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,204 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="login">
+    <section class="left"></section>
+    <section class="right">
+      <form @submit.prevent="login">
+        <div class="logo"><img src="@/assets/logo.png" /></div>
+        <div class="username-password">
+          <input
+            type="text"
+            class="input-text"
+            placeholder="Телефон или email"
+            v-model="username"
+          />
+          <input
+            type="password"
+            class="input-text"
+            placeholder="Пароль"
+            v-model="password"
+          />
+        </div>
+        <div class="forget-submit">
+          <a href="#">Забыли пароль?</a>
+          <input
+            type="submit"
+            value="Войти →"
+            class="btn btn-primary --blue-shadow"
+          />
+        </div>
+        <div class="need-registration">
+          <span> Нет акканта? </span>
+          <a class="btn btn-primary-outline">Регистрация</a>
+        </div>
+      </form>
+    </section>
   </div>
-  <router-view />
 </template>
 
-<style>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      this.$store.dispatch("login", {
+        username: this.username,
+        password: this.password,
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+$font-stack: Arial, "Helvetica Neue", Helvetica, sans-serif;
+$whitish: #fafafa;
+$bluish: #4b6deb;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: $font-stack;
+  width: 100%;
+  height: 100vh;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
 
-#nav {
-  padding: 30px;
+.login {
+  display: grid;
+  grid-template-columns: 0.5fr 1fr;
+  grid-template-rows: 1fr;
+  width: 100%;
+  height: 100%;
+  background: $whitish;
+
+  section.left {
+    grid-column: 1 / 2;
+    grid-row: 1 / 2;
+    background: $bluish;
+  }
+
+  section.right {
+    grid-column: 2 / 3;
+    grid-row: 1 / 2;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    form {
+      width: 30%;
+      height: 50%;
+
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr 1fr 1fr 1fr;
+
+      .logo {
+        grid-column: 1 / 2;
+        grid-row: 1 / 2;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+          height: 90px;
+        }
+      }
+
+      .username-password {
+        grid-column: 1 / 2;
+        grid-row: 2 / 3;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        input {
+          margin: 1em;
+          width: 70%;
+        }
+      }
+
+      .forget-submit {
+        grid-column: 1 / 2;
+        grid-row: 3 / 4;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        a {
+          display: block;
+          font-size: 0.9rem;
+          text-decoration: none;
+          margin-right: 4em;
+          background: none;
+          outline: none;
+          border: none;
+
+          &:hover {
+            color: $bluish;
+          }
+        }
+      }
+
+      .need-registration {
+        grid-column: 1 / 2;
+        grid-row: 4 / 5;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        span {
+          display: block;
+          font-size: 0.8rem;
+          margin-right: 5em;
+        }
+
+        a {
+          display: block;
+          font-size: 0.8rem;
+        }
+      }
+    }
+  }
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+@media (max-width: 768px) {
+  .login {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    overflow: hidden;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+    section.left {
+      display: none;
+    }
+
+    section.right {
+      grid-column: 1 / 2;
+      grid-row: 1 / 2;
+
+      form {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,34 @@
-import { createStore } from "vuex";
+import axios from 'axios';
+import { createStore } from 'vuex';
+// import axios from 'axios';
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    token: localStorage.getItem('token') || null,
+  },
+  mutations: {
+    login(state, access_token) {
+      state.access_token = access_token;
+    },
+  },
+  actions: {
+    async login(ctx, credentials) {
+      axios
+        .post('http://192.168.1.65:8888/kek/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          data: {
+            username: credentials.username,
+            password: credentials.password,
+          },
+        })
+        .then(response => {
+          const access_token = response.data.access_token;
+          localStorage.setItem('token', access_token);
+          ctx.commit('login', access_token);
+        })
+        .catch(error => console.log(error));
+    },
+  },
+  modules: {},
 });
