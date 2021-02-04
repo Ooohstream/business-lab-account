8214e756db23ac3422206fac7de5a3c508229c42
Maksim Dmitriev <lalalabtv@gmail.com>
2021-02-04 10:41:08 +0300

    Create PostLst.vue

---
+++ src/components/layouts/PostLst.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="posts__container" v-for="(post ,key) in posts" :key="key">
+        <PostComponent :title="post.title" :message="post.message" :imageUrl="post.imageUrl" />
+    </div>
+</template>
+
+<script>
+import PostComponent from "@/services/PostComponent"
+
+export default {
+    name: 'PostLst',
+    props:{
+        posts:{
+            type: Array,
+            default: () => []
+        }
+    },
+    components:{
+        PostComponent
+    }
+}
+</script>
