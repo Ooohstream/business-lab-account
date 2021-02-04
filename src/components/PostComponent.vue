53ef70ff324e5e30e591bca5f9b8f83c4e3ef66c
Maksim Dmitriev <lalalabtv@gmail.com>
2021-02-04 10:27:04 +0300

    Update PostComponent.vue

---
+++ src/components/PostComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="post__card">
+        <img :src="imageUrl"/>
+        <div class="post__header">
+            {{title}}
+        </div>
+        <div class="post__details">
+            {{message}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PostCard',
+    props:{
+        title:{
+            type: String,
+            default: ''
+        },
+        message:{
+            type: String,
+            default: ''
+        },
+        imageUrl:{
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
