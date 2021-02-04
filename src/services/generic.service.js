e8caacaa30209d911dca85b2a1440498414573a4
Maksim Dmitriev <lalalabtv@gmail.com>
2021-02-04 14:41:18 +0300

    All done

--- src/services/generic.service.js
+++ src/services/generic.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:3000/api/'
+const API_URL = 'http://localhost:3000/api'
 
 export const request = async ({url, method, data ={}}) => {
     const response = await axios[method](`${API_URL}/${url}`, data);
