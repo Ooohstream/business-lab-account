e74b4132bf350fdd5a97ccd924c68a5f96157e1c
Maksim Dmitriev <lalalabtv@gmail.com>
2021-02-04 10:50:53 +0300

    Update PostLst.vue

--- src/components/layouts/PostLst.vue
+++ src/components/layouts/PostLst.vue
@@ -1,8 +1,11 @@
 <template>
-    <div class="posts__wrapper" v-if="posts.lenght != 0">
+    <div class="posts__if__wrapper" v-if="posts.lenght != 0">
         <div class="posts__container"  v-for="(post ,key) in posts" :key="key">
             <PostComponent :title="post.title" :message="post.message" :imageUrl="post.imageUrl" />
         </div>
+    </div>
+    <div class="posts__else__wrapper" v-else> 
+        <p>Постов нет</p>
     </div>
 </template>
 
