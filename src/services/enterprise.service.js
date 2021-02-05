1d674a72a94e08ae274222edf460c1960323f388
Maksim Dmitriev <lalalabtv@gmail.com>
2021-02-06 00:24:29 +0300

    Update enterprise.service.js

--- src/services/enterprise.service.js
+++ src/services/enterprise.service.js
@@ -1,8 +1,7 @@
-import {request} from "@/services/generic.service"
 import axios from "axios"
 
-const getEnt = (id, token) =>  request({url: `enterprise/onepost` , method: 'get',header:{'access_token':token}, body:{'id':id}})
-const  getEnts = (token) => request({url: `entrerprise/` , method: 'get', header:{'token':token}})
+//const getEnt = (id, token) =>  request({url: `enterprise/onepost` , method: 'get',header:{'access_token':token}, body:{'id':id}})
+//const  getEnts = (token) => request({url: `entrerprise/` , method: 'get', header:{'token':token}})
 
 
 async function createEnt (title,description,token){
@@ -11,6 +10,22 @@ async function createEnt (title,description,token){
         console.log(response)
     })
     console.log(sendEnt)
+}
+
+async function getEnts (token){
+    const config = {headers: {"access_token": `${token}` }, json:true}
+    const takeEnts = await axios.get("http://78.142.222.201:80/api/enterprise/", config).then(response=>{
+        console.log(response)
+    })
+    console.log(takeEnts)
+}
+
+async function getEnt(id, token){
+    const config = {headers: {"access_token": `${token}` }, body:{"id":`${id}`}, json:true}
+    const takeEnts = await axios.get("http://78.142.222.201:80/api/enterprise/onepost", config).then(response=>{
+        console.log(response)
+    })
+    console.log(takeEnts)
 }
 
 export {
