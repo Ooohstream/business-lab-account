7cb81f504b095fec60fa37e8729bf148b5ef865b
Maksim Dmitriev <lalalabtv@gmail.com>
2021-02-05 10:43:14 +0300

    Request Update

--- src/services/generic.service.js
+++ src/services/generic.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = 'http://78.142.222.201:5000/api'
 
-export const request = async ({url, method, data ={}, header={}}) => {
-    const response = await axios[method](`${API_URL}/${url}`, data, header);
+export const request = async ({url, method, data ={}, header={}, body={}}) => {
+    const response = await axios[method](`${API_URL}/${url}`, data, header, body);
     return response.data
 }
--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -1,8 +1,8 @@
 import {request} from "@/services/generic.service"
 
-const getPost = (id, token) =>  request({url: `posts/${id}` , method: 'get',header:{'token':token}})
+const getPost = (id, token) =>  request({url: `posts/onepost` , method: 'get',header:{'token':token}, body:{'id':id}})
 
-const  getPosts = (token) => request({url: `posts` , method: 'get', header:{'token':token}})
+const  getPosts = (token) => request({url: `posts/` , method: 'get', header:{'token':token}})
 
 export {
     getPost,
