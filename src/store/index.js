65441f53359a4dcdfb29b318448f5334273fa745
Ooohstream <69838796+Ooohstream@users.noreply.github.com>
2021-02-06 11:35:37 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -24,7 +24,7 @@ export default createStore({
 
     setUserData(state, userData) {
       state.userData = userData;
-      console.log(state.userData.target.login);
+      console.log(state.userData.get.login);
     },
   },
   actions: {
